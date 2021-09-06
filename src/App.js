f67b4a50a9f98a4037e8638a2a3c0d620561bb6a
maureenv <maureenvogel@outlook.com>
2021-09-06 12:37:08 -0400

    Add images

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import React, { useState } from 'react'
+import styled from '@emotion/styled'
 import logo from './images/logo-green-white.png'
 import video from './images/video.mp4'
-import bg1 from './images/2.jpg'
+import bg1 from './images/1.jpg'
+import bg2 from './images/2.jpg'
+import bg3 from './images/3.jpg'
+import bg4 from './images/4.jpg'
 import './styles.css'
 
 const claims = [
   {
     claim: 'Recycling your cartons is as easy as 1-2-3: Empty your cartons, toss them in the recycling bin, take your bin to the curb.',
     fact: 'According to Tetra Pak, carton recycling is available to 73 million U.S. households. Even if that number is accurate, it still leaves about 50 million households without any carton recycling options at all. However, even in areas which offer curbside pickup for cartons, those cartons are not typically processed locally, but instead baled and shipped thousands of miles to specialized facilities in Mexico and elsewhere, or simply landfilled.',
-    bg: require( './images/2.jpg' ),
+    bg: bg1,
   },
   {
     claim: 'The principles of circularity are at the very heart of the carton business.',
     fact: 'Cartons are broken down into separate materials, including paper, plastic and aluminum, and from there can be turned into paper towels and other single-use paper products or building materials such as ceiling and wallboard. This is not, by definition, a circular economy, but instead a manufacture of single-use products out of leftover material. Cartons require virgin materials to be constructed, and recycled cartons do not end up back on the shelf as new ones.',
-    bg: require( './images/2.jpg' ),
+    bg: bg2,
   },
   {
     claim: 'Around the world, one million tons of used beverage cartons are recycled every year.',
     fact: 'Various estimates have found that cartons are recycled at an extremely low rate, likely below 20 percent, which is even worse than plastic. For the one million tons of recaptured cartons, hundreds of millions are going to the landfill. Tetra Pak alone produces more than 300 billion new food and beverage cartons annually and has become the largest food and beverage packaging company in the world.',
-    bg: require( './images/2.jpg' ),
+    bg: bg3,
   },
   {
     claim: 'Cartons produce five times less overall climate impact than other forms of packaging.',
     fact: 'Carton industry claims about reduced emissions relative to other forms of packaging examine only the cost of new containers and ignore the emissions reduction benefits of recycled containers. In short, the end-of-life portion of the life cycle is completely ignored. If assessments do not examine emissions based on current recycling rates and recycled material inputs, they do not accurately reflect the climate change impact of various container types.',
-    bg: require( './images/2.jpg' ),
+    bg: bg4,
   },
 ]
 
@@ -60,7 +64,7 @@ const App = () => {
           <h2 className="section-heading">Carton claims debunked</h2>
           <div className="cards-container">
             { claims.map( (c, i) =>
-              <div className={ showFact[i] ? 'fact-card' : 'claim-card' } style={{ backgroundImage: c.bg }}>
+              <Card showFact={ showFact[i] } background={ c.bg }>
               { showFact[i] ?
                 <>
                   <p className="claim">Fact</p>
@@ -74,7 +78,7 @@ const App = () => {
                   <button onClick={ () => onShowFact( i ) } className="claim-button">See Fact</button>
                 </>
               }
-              </div>
+              </Card>
             )}
           </div>
         </div>
@@ -83,5 +87,37 @@ const App = () => {
   )
 }
 
+const Card = styled.div`
+  /*background: ${ props => props.showFact ? '#106759' : 'black' };*/
+  background: url( ${ props => props.background }) no-repeat center;
+  background-size: cover;
+  border-radius: 4px;
+  overflow: hidden;
+  z-index: 1;
+  position: relative;
+  color: #fff;
+  font-family: 'regular';
+  padding: 25px;
+  width: 48%;
+  height: 250px;
+  margin-bottom: 20px;
+  line-height: 1.4;
+  border-radius: 4px;
+  box-sizing: border-box;
+  font-size: ${ props => props.showFact ? '14px' : '18px' };
+  transition: 0.2s ease-in-out opacity;
+  &:after {
+    content: '';
+    transition: 0.2s ease-in-out opacity;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    top: 0;
+    background: ${ props => props.showFact ? '#106759' : 'black'};
+    opacity: ${ props => props.showFact ? 1 : 0.8 };
+    position: absolute;
+    z-index: -1;
+  }
+`
 
 export default App
