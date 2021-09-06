18c8a504104e9d8ff598dd85f3fc5b9cf088d9ad
maureenv <maureenvogel@outlook.com>
2021-09-06 04:27:23 -0400

    Add video bg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './images/logo-green-white.png'
 import video from './images/video.mp4'
-import './App.css';
+import './styles.css'
 
 function App() {
   return (
@@ -9,6 +9,11 @@ function App() {
       <div className="hero-video">
         <div className="video-container">
           <video playsInline autoPlay muted loop src={ video } type="video/mp4"/>
+        </div>
+        <div className="hero-copy">
+          <h1>Are cartons actually recyclable?</h1>
+          <h2>Marketed as recyclable but treated by recycling programs as garbage and landfilled...</h2>
+          <button className="cta">Learn the truth</button>
         </div>
       </div>
     </div>
